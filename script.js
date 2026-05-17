04cf304f216b4f793f8a52a0b2d5a8f64d2de196
Adarshtrivedi09 <adarshtrivedi838@gmail.com>
2026-05-17 22:18:21 +0530

    enhanced css and created theme mode button

---
+++ script.js
@@ -0,0 +1,24 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('nav');
+
+menuToggle.addEventListener('click', () => {
+    nav.classList.toggle('active');
+});
+
+
+const hiddenElements = document.querySelectorAll(".hidden");
+
+const observer = new IntersectionObserver((entries) => {
+    
+    entries.forEach((entry) => {
+
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+
+    });
+
+});
+
+hiddenElements.forEach((el) => observer.observe(el));
+
